6121a1bf6a86e14eacac2f577f98ceb92512d270
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-01 18:36:49 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -16,10 +16,18 @@ class Personagem(pygame.sprite.Sprite, Fisica):
         self.fisica = Fisica()
         self.__vida = 100
         self.retangulo = pygame.Rect(x, y, 50, 50)
+        self.flip = 0
     
     @property
     def vida(self):
         return self.__vida
+    
+    @vida.setter
+    def vida(self, val:int):
+        if val < 0:
+            self.__vida = 0
+        else:
+            self.__vida = val
     
     def actions(self, altura, largura, tela, alvo, op: int):
         teclas = pygame.key.get_pressed()
@@ -41,6 +49,12 @@ class Personagem(pygame.sprite.Sprite, Fisica):
 
             if teclas[pygame.K_r]:
                 self.attack(tela, alvo)
+                
+            if alvo.retangulo.centerx > self.retangulo.centerx:
+                self.flip = 1
+            else:
+                self.flip = 0
+                
 
             self.retangulo.y = self.fisica.aplicar_gravidade(self.retangulo.y, altura - self.retangulo.height)
         
@@ -58,18 +72,29 @@ class Personagem(pygame.sprite.Sprite, Fisica):
             if teclas[pygame.K_w] and not self.fisica.pulo and self.retangulo.y == altura - self.retangulo.height:
                 self.fisica.iniciar_pulo()
 
-            if teclas[pygame.K_r]:
+            if teclas[pygame.K_f]:
                 self.attack(tela, alvo)
+                
+            if alvo.retangulo.centerx > self.retangulo.centerx:
+                self.flip = 0
+            else:
+                self.flip = 1
 
             self.retangulo.y = self.fisica.aplicar_gravidade(self.retangulo.y, altura - self.retangulo.height)
     
     def attack(self, superficie, alvo):
-        attacking_rect = pygame.Rect(self.retangulo.centerx, self.retangulo.y, 2 * self.retangulo.width, self.retangulo.height)
+        attacking_rect = pygame.Rect(self.retangulo.centerx - (2 *  self.retangulo.width * self.flip), self.retangulo.y, 2 * self.retangulo.width, self.retangulo.height)
         if attacking_rect.colliderect(alvo.retangulo):
-            print("Acertou")
+            alvo.vida -= 10 
          
         
-    
+    def barra_vida(self, vida, x, y):
+        razao = vida / 100
+        pygame.draw.rect(tela, (255, 255, 255), (x - 2, y - 2, 304, 34))
+        pygame.draw.rect(tela, (255, 0, 0), (x, y, 300, 30))
+        pygame.draw.rect(tela, (0, 255, 0), (x, y, 300 * razao, 31))
+        
+        
     def desenhar(self, superficie):
         pygame.draw.rect(superficie, (255, 0, 0), self.retangulo)
 
@@ -84,12 +109,16 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            sys.exit()
+            
 
-    tela.fill((255, 255, 255))    
+    tela.fill((0, 0, 0))
     player.actions(altura, largura, tela, player2, 1)      
     player.desenhar(tela)
-    player2.actions(altura, largura, tela, player, 2)      
+    player.barra_vida(player.vida, 10, 20)
+    player2.actions(altura, largura, tela, player, 2)    
     player2.desenhar(tela)     
+    player.barra_vida(player2.vida, 490, 20)
     pygame.display.flip() 
             
 pygame.quit()
