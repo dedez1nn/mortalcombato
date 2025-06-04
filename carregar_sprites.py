063cd5bb833b06122787647fd3980662a1c861ad
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-04 18:58:56 -0300

    Setters e getters

--- carregar_sprites.py
+++ carregar_sprites.py
@@ -1,68 +1,78 @@
 import pygame
 
-class Fisica:
-    def __init__(self):
-        self.__vel_y = 0
-        self.__gravidade = 1
-        self.__altura_pulo = 20
-        self.__no_ar = False
-        self.__velocidade = 5
-
-    @property
-    def velocidade(self):
-        return self.__velocidade
-    
-    @velocidade.setter
-    def velocidade(self, val: int):
-        self.__velocidade = val
+class Sprites:
+    #(cris_tonaldo, [3, 4, 5, 6 ,7])
+    def __init__(self, nome:str, numanim: list):
+    #numidles: int, numwalk: int, numjump:int, numjkick:int, numkick:int, numpunch:int
+        self.__sprites = {}
+        self.__listanm = numanim
+        self.__nomep = nome
+        self.__sprite_atual = 0
         
+    #0 idle 
+    #1 walk
+    #2 jump
+    #3 punch
+    #4 kick
+    #5 block
+    #6 damage
+    #7 jumpkick
+    
     @property
-    def no_ar(self):
-        return self.__no_ar
-
-    @no_ar.setter
-    def pulo(self, valor: bool):
-        self.__no_ar = valor
-
+    def sprites(self):
+        return self.__sprites
+    
+    @property 
+    def listanm(self):
+        return self.__listanm
+    
     @property
-    def vel_y(self):
-        return self.__vel_y
-
-    @vel_y.setter
-    def vel_y(self, valor: float):
-        self.__vel_y = valor
-
+    def sprite_atual(self):
+        return self.__sprite_atual
+    
     @property
-    def gravidade(self):
-        return self.__gravidade
-
-    @gravidade.setter
-    def gravidade(self, valor: float):
-        self.__gravidade = valor
-
-    @property
-    def altura_pulo(self):
-        return self.__altura_pulo
-
-    @altura_pulo.setter
-    def altura_pulo(self, valor: float):
-        self.__altura_pulo = valor
-
-    def aplicar_gravidade(self, y, limite_chao):
-        if self.__no_ar:
-            y += self.vel_y
-            self.vel_y += self.gravidade
-
-            if y >= limite_chao:
-                y = limite_chao
-                self.__no_ar = False
-                self.__vel_y = 0
-
-        return y
-
-    def iniciar_pulo(self):
-        self.__no_ar = True
-        self.__vel_y = -self.__altura_pulo
+    def nomep(self):
+        return self.__nomep
+    
+    @sprite_atual.setter
+    def sprite_atual(self, val: float):
+        if val <= 0: #tratamento
+            self.__sprite_atual = 0.1
+        else:
+            self.__sprite_atual = val
+        
+    @sprites.setter
+    def sprites(self, val: dict):
+        self.__sprites = val
+        
+    @listanm.setter
+    def listanm(self, val: list):
+        self.__listanm = val
+    
+    @nomep.setter
+    def nomep(self, novo: str):
+        self.__nomep = novo
+        
+    def addsprites(self):
+        for i in range(len(self.__listanm)): # tratar exceções antes
+            for j in range(self.__listanm[i]):
+                temp = pygame.image.load(f'{self.nomep}/{i}/{j}.png').convert_alpha()
+                temp = pygame.transform.scale(temp, (150, 150))
+                self.__sprites[(i, j)] = temp
+                
+    def percorrer(self, indice: int):
+        self.sprite_atual += 0.2
+        if self.sprite_atual >= self.__listanm[indice]:
+            self.sprite_atual = 0
+        temp = self.__sprites[(indice, int(self.sprite_atual))]
+        return temp
+    
+    def esperar_sprite(self, indice: int):
+        pass
+        
+         
+            
+         
             
             
-            
+    
