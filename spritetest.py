1d137ffe811d9bed23f36c0bbcb2e49b48e40702
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-04 08:54:02 -0300

    Create spritetest.py
    Teste para implementação da adição de sprites

---
+++ spritetest.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Import(pygame.sprite.Sprite):
+    def __init(self, nome:str, numanim: list):
+    #numidles: int, numwalk: int, numjump:int, numjkick:int, numkick:int, numpunch:int
+        self.__sprites = {}
+        self.__listanm = numanim
+        
+    def addsprites(self):
+        for i in range(self.listanm[0]):
+            temp = pygame.image.load('')
+    
+    
