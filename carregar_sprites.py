53082da520d84c9eba3c72062b0ff6684e8550c7
GabryelDEVNewton <161973121+GabryelDEVNewton@users.noreply.github.com>
2025-06-04 22:05:52 -0300

    Update carregar_sprites.py

--- carregar_sprites.py
+++ carregar_sprites.py
@@ -1,78 +1,31 @@
 import pygame
 
 class Sprites:
-    #(cris_tonaldo, [3, 4, 5, 6 ,7])
-    def __init__(self, nome:str, numanim: list):
-    #numidles: int, numwalk: int, numjump:int, numjkick:int, numkick:int, numpunch:int
+    def __init__(self, nome: str, numanim: list):
         self.__sprites = {}
         self.__listanm = numanim
         self.__nomep = nome
-        self.__sprite_atual = 0
-        
-    #0 idle 
-    #1 walk
-    #2 jump
-    #3 punch
-    #4 kick
-    #5 block
-    #6 damage
-    #7 jumpkick
-    
-    @property
-    def sprites(self):
-        return self.__sprites
-    
-    @property 
-    def listanm(self):
-        return self.__listanm
-    
-    @property
-    def sprite_atual(self):
-        return self.__sprite_atual
-    
+        self.sprite_atual = 0.0  # Mantido como float para controle fino
+
     @property
     def nomep(self):
         return self.__nomep
-    
-    @sprite_atual.setter
-    def sprite_atual(self, val: float):
-        if val <= 0: #tratamento
-            self.__sprite_atual = 0.1
-        else:
-            self.__sprite_atual = val
-        
-    @sprites.setter
-    def sprites(self, val: dict):
-        self.__sprites = val
-        
-    @listanm.setter
-    def listanm(self, val: list):
-        self.__listanm = val
-    
+
     @nomep.setter
     def nomep(self, novo: str):
         self.__nomep = novo
-        
+
     def addsprites(self):
-        for i in range(len(self.__listanm)): # tratar exceções antes
+        for i in range(len(self.__listanm)):
             for j in range(self.__listanm[i]):
                 temp = pygame.image.load(f'{self.nomep}/{i}/{j}.png').convert_alpha()
                 temp = pygame.transform.scale(temp, (150, 150))
                 self.__sprites[(i, j)] = temp
-                
+
     def percorrer(self, indice: int):
-        self.sprite_atual += 0.2
+        # Diminui a velocidade da animação ajustando a taxa de avanço
+        self.sprite_atual += 0.1  # quanto menor, mais lenta
         if self.sprite_atual >= self.__listanm[indice]:
             self.sprite_atual = 0
         temp = self.__sprites[(indice, int(self.sprite_atual))]
         return temp
-    
-    def esperar_sprite(self, indice: int):
-        pass
-        
-         
-            
-         
-            
-            
-    
