faab70df0858d682cce7a2a81d22a989b1f9b96d
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-05 13:05:57 -0300

    Variavel fim para pegar o fim de sprite para controlar o cooldown

--- carregar_sprites.py
+++ carregar_sprites.py
@@ -1,31 +1,85 @@
 import pygame
 
 class Sprites:
-    def __init__(self, nome: str, numanim: list):
+    #(cris_tonaldo, [3, 4, 5, 6 ,7])
+    def __init__(self, nome:str, numanim: list):
+    #numidles: int, numwalk: int, numjump:int, numjkick:int, numkick:int, numpunch:int
         self.__sprites = {}
         self.__listanm = numanim
         self.__nomep = nome
-        self.sprite_atual = 0.0  # Mantido como float para controle fino
-
+        self.__sprite_atual = 0
+        self.__sprite_fim = 0
+        
+    #0 idle 
+    #1 walk
+    #2 jump
+    #3 punch
+    #4 kick
+    #5 block
+    #6 damage
+    #7 jumpkick
+    
+    @property
+    def sprites(self):
+        return self.__sprites
+    
+    @property 
+    def listanm(self):
+        return self.__listanm
+    
+    @property
+    def sprite_fim(self):
+        return self.__sprite_fim
+    
+    @property
+    def sprite_atual(self):
+        return self.__sprite_atual
+    
     @property
     def nomep(self):
         return self.__nomep
-
+    
+    @sprite_atual.setter
+    def sprite_atual(self, val: float):
+        if val <= 0: #tratamento
+            self.__sprite_atual = 0.1
+        else:
+            self.__sprite_atual = val
+        
+    @sprites.setter
+    def sprites(self, val: dict):
+        self.__sprites = val
+        
+    @listanm.setter
+    def listanm(self, val: list):
+        self.__listanm = val
+    
     @nomep.setter
     def nomep(self, novo: str):
         self.__nomep = novo
-
+        
+    @sprite_fim.setter
+    def sprite_fim(self, val: int):
+        self.__sprite_fim = val
+        
     def addsprites(self):
-        for i in range(len(self.__listanm)):
+        for i in range(len(self.__listanm)): # tratar exceções antes
             for j in range(self.__listanm[i]):
                 temp = pygame.image.load(f'{self.nomep}/{i}/{j}.png').convert_alpha()
                 temp = pygame.transform.scale(temp, (150, 150))
                 self.__sprites[(i, j)] = temp
-
-    def percorrer(self, indice: int):
-        # Diminui a velocidade da animação ajustando a taxa de avanço
-        self.sprite_atual += 0.1  # quanto menor, mais lenta
-        if self.sprite_atual >= self.__listanm[indice]:
+                
+    def percorrer(self, indice: int, vel: float):
+        
+        self.sprite_atual += vel
+        if self.sprite_atual >= self.listanm[indice]:
             self.sprite_atual = 0
-        temp = self.__sprites[(indice, int(self.sprite_atual))]
+            self.sprite_fim = 1
+        else:
+            self.sprite_fim = -1
+        temp = self.sprites[(indice, int(self.sprite_atual))]
         return temp
+        
+         
+            
+         
