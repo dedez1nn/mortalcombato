82e4e1cb7730c28f9de4325c0d34fed07d5e9ced
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-10 23:35:38 -0300

    Update inimigo.py

--- inimigo.py
+++ inimigo.py
@@ -5,63 +5,73 @@ from carregar_sprites import Sprites
 from estado_animacoes import Estados
 from personagem_base import PersonagemBase
 
-class Inimigo1(PersonagemBase):
-    def __init__(self, nome: str, listar: list, x: int, y: int):
+class Inimigo(PersonagemBase):
+     def __init__(self, nome: str, listar: list, x: int, y: int):
         super().__init__(nome, listar, x, y)
-        self.__tempo_recuo = 0
-        self.__recua = False
-
-    @property
-    def tempo_recuo(self):
-        return self.__tempo_recuo
-    
-    @tempo_recuo.setter
-    def tempo_recuo(self, val: int):
-        self.__tempo_recuo = val
+        self.__cooldownd = 0
+        self.__cooldown_block = 0
         
-    @property
-    def recua(self):
-        return self.__recua
+     @property
+     def cooldownd(self):
+        return self.__cooldownd
     
-    @recua.setter
-    def recua(self, val: bool):
-        self.__recua = val
-
+     @cooldownd.setter
+     def cooldownd(self, val: int):
+        self.__cooldownd = val 
         
-    def actions(self, superficie, altura, largura, alvo):
-        tempo_atual = pygame.time.get_ticks()
-
+     @property
+     def cooldown_block(self):
+        return self.__cooldown_block
+    
+     @cooldown_block.setter
+     def cooldown_block(self, val: int):
+        self.__cooldown_block = val 
+        
+     def actions(self, superficie, altura, largura, alvo):
+        deslocamento = 0
+            
+        if self.retangulo.x > alvo.retangulo.x:
+            self.flip = True
+            deslocamento = -2
+        else:
+            self.flip = False
+            deslocamento = 2
+        
+        if self.estados.atingido:
+            self.retangulo.x += -(deslocamento * 2.5)
+            
+        if self.retangulo.x <= 0:
+            self.retangulo.x = 0
+            self.estados.andando = False
         if self.retangulo.right >= largura:
             self.retangulo.right = largura
-        elif self.retangulo.x <= 0:
-            self.retangulo.x = 0
+            self.estados.andando = False
 
-        if self.retangulo.x < alvo.retangulo.x:
-            self.flip = False
-            distanciaplayer = self.retangulo.right - alvo.retangulo.left
-            deslocamento = 2
-        else:
-            self.flip = True
-            distanciaplayer = self.retangulo.left - alvo.retangulo.right
-            deslocamento = -2
-
-        if not (self.estados.atingido or self.estados.soco or self.estados.pulando):
-            if self.recua:
-                if tempo_atual - self.tempo_recuo < 500:  # recuar por 0.5 segundos
-                    self.retangulo.x -= deslocamento  # move na direção oposta
-                    self.estados.andando = True
-                else:
-                    self.recua = False  # terminou de recuar
-
-            elif distanciaplayer > 0 and not self.retangulo.colliderect(alvo.retangulo):
-                self.retangulo.x += deslocamento
-                self.estados.andando = True
-
+        if not self.estados.atingido and not self.estados.soco and not self.estados.pulando:
+            if self.retangulo.colliderect(alvo.retangulo) and self.__cooldownd == 0:    
+                if self.ataquecdr == 0:
+                    self.estados.soco = True         
+                    self.estados.andando = False 
+                    self.cooldownd = 40             
+            elif self.cooldownd > 0:
+                if not self.estados.defesa:
+                    self.retangulo.x += -deslocamento * (1/2)    
+                    self.estados.andando = True      
+                if self.cooldownd <= 10 and self.cooldown_block == 0:
+                    self.estados.defesa = True                    
             else:
-                self.recua = True
-                self.tempo_recuo = tempo_atual
-                self.estados.andando = True
+                if not self.estados.defesa and not self.retangulo.colliderect(alvo.retangulo):
+                    self.estados.andando = True              
+                    self.retangulo.x += deslocamento
+            
+        if self.ataquecdr > 0:
+            self.ataquecdr -= 0.25
+        if self.__cooldownd > 0:
+            self.__cooldownd -= 0.25
+        if self.__cooldown_block > 0:
+            self.__cooldown_block -= 0.25
+            
 
-        self.atualizar_animacao(superficie, alvo)
+        self.atualizar_animacao(superficie, alvo, 60) #superficie, player, cdratk
         self.estados.resetar_estados()
         
