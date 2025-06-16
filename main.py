1bddbbe6f434b71cdf29110683005e7be2597a8a
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-06-16 17:56:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from carregar_sprites import Sprites
 from jogador import Jogador
 from inimigo import Inimigo
 from fase import Fase
+from tela_jogar import Tela_Jogar
 
 
 def main():
@@ -17,15 +18,17 @@ def main():
     largura = 800
     altura = 600
     tela = pygame.display.set_mode((largura, altura))
+    fonte = pygame.font.SysFont(None, 38)
 
     pygame.display.set_caption("pescocoviado")
 
 
-
+    fundo = pygame.image.load("fundoo.jpg").convert_alpha()
     run = True
     player = Jogador("cris_tonaldo", [3, 3, 4, 3, 4, 1, 8, 5], 10, altura - 150)
     player2 = Inimigo("cris_tonaldo", [3, 3, 4, 3, 4, 1, 8, 5], 400, altura - 150)
     clock = pygame.time.Clock()
+    telatest = Tela_Jogar(player, player2, fundo, 50)
     fase = Fase(player, player2)
 
     while run:
@@ -35,14 +38,20 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 sys.exit()
+            '''elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    player.pause()'''
+                            
                 
         tela.fill((0, 0, 0))
         fase.loopmain()
-        player.actions(tela, altura, largura, player2)
+        telatest.tela_fighting(tela, altura, largura, fonte, fundo)
+        '''player.actions(tela, altura, largura, player2)
         #(self, superficie, vida, x, y)
-        player.barra_vida(tela, player.vida, 10, 20)
+        player.barra_vida(tela, 10, 20)
         player2.actions(tela, altura, largura, player) 
-        player2.barra_vida(tela, player2.vida, 490, 20)
+        player2.barra_vida(tela, 490, 20)'''
+        
         pygame.display.flip() 
                 
     pygame.quit()
