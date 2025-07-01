dbe9269d7a1cb84cdf1b213994b5ffaf8844de10
dedez1nn <153863098+dedez1nn@users.noreply.github.com>
2025-07-01 18:03:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from jogador_ranking import JogadorRanking
 from tela_inicial import Tela_Inicial
 from menu import Menu
 import sys
+from elem_independente import Elem_Independente
+from carregar_sprites import Sprites
 
 def main():
     pygame.init()
@@ -20,15 +22,17 @@ def main():
     largura = 800
     altura = 600
     tela = pygame.display.set_mode((largura, altura))
+    nuvem = pygame.image.load("assets/elem_independente/nuvem.png").convert_alpha()
+    nuvem = pygame.transform.scale(nuvem, (200, 100))
     fonte = pygame.font.Font("MKX Title.ttf", 38)
     fonte_titulo = pygame.font.Font("MKX Title.ttf", 50)
-    fundo = pygame.image.load("fundo/1.png").convert_alpha()
+    fundo = pygame.image.load("assets/fundoo.jpg").convert_alpha()
     pygame.display.set_caption("pescocoviado")
-    musica = "MusicaMenu.mp3"
-    caminho_fundo = "fundo/1.png"
+    musica = "assets/MusicaMenu.mp3"
+    caminho_fundo = "assets/fundoo.jpg"
     inicio = Tela_Inicial(caminho_fundo)
     inicio.inicial(tela, fonte_titulo, fonte)
-    nomejogador = inicio.get_nome_jogador()
+    nomejogador = inicio.nome_jogador
 
     # Dados dos personagens
     sprites_tonaldo = [3, 3, 4, 3, 4, 1, 8, 5]
@@ -53,18 +57,25 @@ def main():
             lista_sons_select.append(pygame.mixer.Sound(f"assets/tela_select/sons/{lista_personagens[i]}.mp3"))
     except Exception as e:
         print(e)
+    menu = Menu(245, 55, lista_personagens, lista_mini, lista_default, lista_sons_select, sofreu_dano, lista_n_sprites)
+    clock = pygame.time.Clock()
+    run = True
 
     menu = Menu(245, 55, lista_personagens, lista_mini, lista_default, lista_sons_select, sofreu_dano, lista_n_sprites)
-    fundo_menu = menu.fundo_tela(800, 600, "fundo/1.png")
+    fundo_menu = menu.fundo_tela(800, 600, "assets/fundoo.jpg")
     menu.titulo_menu(fonte_titulo, "Mortal Kombat", (0, 0, 0))
     menu.criar_botao(fonte)
     menu.tocar_musica(musica)
+    
+    
+    elem_inds = Elem_Independente(largura, nuvem)
 
     clock = pygame.time.Clock()
     run = True
 
     while run:
         clock.tick(60)
+        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -83,7 +94,7 @@ def main():
                 res = select.exibir(tela, fonte, fundo)
                 nome = lista_personagens[res]
                 tam_sprites = lista_n_sprites[res]
-                menu.tocar_musica("MusicaBatalha.mp3")
+                menu.tocar_musica(musica)
                 menu.volume_ef = select.volume_ef
                 menu.volume_mus = select.volume_mus
 
@@ -95,25 +106,22 @@ def main():
                 sprites_bot = lista_n_sprites[sorteio]
                 bot = Inimigo(nome_bot, sprites_bot, 400, altura - 150)
 
-                jogar = Tela_Jogar(player, bot, fundo, menu.volume_ef * 100, menu.volume_mus * 100)
+                jogar = Tela_Jogar(player, bot, fundo, menu.volume_ef * 100, menu.volume_mus * 100, elem_inds)
                 sorteio = random.randint(1, 2)
 
                 res = jogar.tela_fighting(tela, altura, largura, fonte, fundo, sorteio)
                 res_pausa = jogar.pausa(tela, fonte, fundo, res)
 
                 if res == 0 or res_pausa == 0:
-
                     menu.volume_ef = jogar.volume_ef
-                    menu.volume_mus = jogar.volume_mus
-                    pontos, sequencia = jogar.fase.get_dados_finais()                
+                    menu.volume_mus = jogar.volume_mus              
                     manager_ranking = ManagerRanking()
                     manager_ranking.carrega_arquivo("ranking.json")
-                    manager_ranking.atualizar_ou_adicionar(JogadorRanking(nomejogador, pontos, sequencia))
                     manager_ranking.salvar_ranking("ranking.json")
                     break
 
             elif clique == 1:
-                ranking = Tela_Ranking("fundo/1.png")
+                ranking = Tela_Ranking("assets/fundoo.jpg")
                 ranking.ranking(tela, fonte, fonte_titulo)
 
             elif clique == 2:
@@ -124,7 +132,7 @@ def main():
                 run = False
                 pygame.quit()
                 sys.exit()
-
+ 
         tela.blit(fundo_menu, (0, 0))
         menu.desenha_titulo_menu(tela)
         for botao in menu.botoes:
